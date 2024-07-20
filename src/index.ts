6fbba73bce317578ebbbba5187aa028326d305a0
Tomu Maekawa <tomu.m1107@icloud.com>
2024-07-20 18:38:38 +0900

    expressでのローカルサーバー立ち上げと、mongoDBの接続ができた

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+const PORT = 3002
+const userRoutes = require("./routes/users")
+const authRoutes = require("./routes/auth")
+const postsRoutes = require("./routes/posts")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
+const monogURL = process.env.MONGO_URL
+
+mongoose.connect(monogURL as string)
+  .then(() => console.log("DB接続中..."))
+  .catch((err: Error) => console.log(err))
+
+app.use(express.json())
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postsRoutes);
+
+app.listen(PORT, () => {
+  console.log("サーバーが立ち上がっています")
+})
