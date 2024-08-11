821627aa3ea6896aad70b05e55fb640cfd37ebc5
Tomu Maekawa <tomu.m1107@icloud.com>
2024-08-12 02:28:56 +0900

    サーバー側のディレクトリは完成した。デプロイします

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,66 +1,93 @@
-import { timeStamp } from "console";
-import { Document, Schema, SchemaDefinition, model } from "mongoose";
+import { Document, Schema, model } from 'mongoose';
 
 interface IPost extends Document {
-  userId: string,
-  name: string,
-  event: string,
-  region: string,
-  date: string,
+  customId: string;
+  userId: string;
+  name: string;
+  event: string;
+  region: string;
+  startDate: string;
+  endDate: string;
+  completed: boolean;
   mypage: {
-    url: string,
-    id: string,
-    password: string,
-  },
-  taskFlow: [{
-    task: string,
-    situation: string,
-    testFormat: string,
-    date: string,
-    limitData:string,
-  }],
-  createdAt: Date,
-  updatedAt: Date,
+    url: string;
+    id: string;
+    password: string;
+  };
+  taskFlow: [
+    {
+      customId: string;
+      task: string;
+      situation: string;
+      testFormat: string;
+      date: string;
+      limitDate: string;
+      current: boolean;
+      next: boolean;
+      finished: boolean;
+      edit: boolean;
+    },
+  ];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const PostSchema = new Schema<IPost>({
-  userId: {
-    type: String,
-    required: true
+const PostSchema = new Schema<IPost>(
+  {
+    customId: { type: String, required: true, unique: true },
+    userId: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true,
+      max: 40,
+    },
+    event: {
+      type: String,
+      // required: true,
+    },
+    region: {
+      type: String,
+      // required: true,
+      max: 20,
+    },
+    startDate: {
+      type: String,
+      required: true
+    },
+    endDate: {
+      type: String,
+      required: true
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    mypage: {
+      url: { type: String, max: 80, default: '' },
+      id: { type: String, max: 40, default: '' },
+      password: { type: String, max: 40, default: '' },
+    },
+    taskFlow: [
+      {
+        customId: { type: String },
+        task: { type: String },
+        situation: { type: String, default: '未完了' },
+        testFormat: { type: String, max: 20 },
+        date: { type: String },
+        limitDate: { type: String },
+        current: { type: Boolean, default: false },
+        next: { type: Boolean, default: false },
+        finished: { type: Boolean, default: false },
+        edit: { type: Boolean, default: false },
+      },
+    ],
   },
-  name: {
-    type: String,
-    required: true,
-    max: 40,
-  },
-  event: {
-    type: String,
-    required: true,
-  },
-  region: {
-    type: String,
-    required: true,
-    max: 20,
-  },
-  date: {
-    type: String,
-    required: true
-  },
-  mypage: {
-    url: {type: String, max: 80},
-    id: {type: String, max: 40 },
-    password: {type: String, max: 40}
-  },
-  taskFlow: [{
-    task: {type: String},
-    situation: {type: String},
-    testFormat: {type: String, max:20},
-    date: {type: String},
-    limitDate: {type: String},
-  }],
-},
-{
-  timestamps: true
-})
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = model<IPost>('Post', PostSchema)
+module.exports = model<IPost>('Post', PostSchema);
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,33 +1,34 @@
-import { Request, Response } from "express";
-const User = require("../models/User")
 
-const router = require("express").Router();
+import { Request, Response } from 'express';
+// import User from '../models/User';
+const User = require('../models/User')
+const router = require('express').Router();
 
-router.post("/register", async (req:Request, res:Response) => {
+router.post('/register', async (req: Request, res: Response) => {
   try {
     const newUser = await new User({
       username: req.body.username,
-      email: req.body.email,
       password: req.body.password,
     });
 
     const user = await newUser.save();
-    return res.status(200).json(user)
+    return res.status(200).json(user);
   } catch (error) {
-    return res.status(500).json(error)
+    return res.status(500).json(error);
   }
-})
+});
 
-router.post("/login", async (req:Request, res:Response) => {
+router.post('/login', async (req: Request, res: Response) => {
   try {
-    const user = await User.findOne({email: req.body.email});
-    if(!user) return res.status(404).send("ユーザーが見つかりません");
+    const user = await User.findOne({ username: req.body.username});
+    if (!user) return res.status(404).send('ユーザーが見つかりません');
     const vaildPassword = req.body.password === user.password;
-    if(!vaildPassword) return res.status(400).send("パスワードが見つかりません");
-    return res.status(200).json(user)
+    if (!vaildPassword)
+      return res.status(400).send('パスワードが見つかりません');
+    return res.status(200).json(user);
   } catch (error) {
-    return res.status(500).json(error)
+    return res.status(500).json(error);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
