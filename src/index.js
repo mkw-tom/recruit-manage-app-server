9862c1d2718833d641da5703c3c2a98e69da238e
Tomu Maekawa <tomu.m1107@icloud.com>
2024-08-12 03:33:34 +0900

    tsconfigの設定を加えてデプロイエラーを修正

---
+++ src/index.js
@@ -0,0 +1,24 @@
+"use strict";
+const express = require('express');
+const app = express();
+const PORT = 3002;
+const userRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth');
+const postsRoutes = require('./routes/posts');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config();
+const monogURL = process.env.MONGO_URL;
+mongoose
+    .connect(monogURL)
+    .then(() => console.log('DB接続中...'))
+    .catch((err) => console.log(err));
+app.use(cors());
+app.use(express.json());
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postsRoutes);
+app.listen(PORT, () => {
+    console.log('サーバーが立ち上がっています');
+});
